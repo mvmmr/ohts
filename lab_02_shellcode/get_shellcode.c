01012d32bfa69edc6416daaf58bc420cf465dc59
muammar <muammararif007@gmail.com>
2020-03-11 14:24:38 +0530

    shellcode: add comments

--- lab_02_shellcode/get_shellcode.c
+++ lab_02_shellcode/get_shellcode.c
@@ -4,20 +4,28 @@
 
 int main(int argc, char *argv[])
 {
-		char l = 1;
-		unsigned char buf;
-		int fd;
-		fd = open(argv[1],0,S_IRUSR);
-		while(read(fd,&buf,1)) {
-				if(buf == 0 && l == 1) {
-						printf(" \n");
-						l = 0;
-				}
-				else if(buf) {
-						printf("\\x%02x",buf);
-						l = 1;
-				}
+	char l = 1;
+	unsigned char buf;
+	int fd;
+
+	// Open program from CLI arguments
+	fd = open(argv[1], 0, S_IRUSR);
+
+	while(read(fd, &buf, 1))
+	{
+		// Ignore null bytes
+		if (buf == 0 && l == 1) {
+			printf(" \n");
+			l = 0;
 		}
 
-		close(fd);
+		// Print formatted shellcode
+		else if (buf) {
+			printf("\\x%02x", buf);
+			l = 1;
+		}
+	}
+
+	// close file handler
+	close(fd);
 }
